8833ad9725c4b94046aca04feade0a1793c7f5b3
teguh02 <teguhrijanandi02@gmail.com>
2020-11-30 17:03:00 +0700

    httpRequest Improvment

--- src/helper/httpRequest.php
+++ src/helper/httpRequest.php
@@ -11,12 +11,15 @@ class httpRequest extends mobilepulsa {
      */
     public static function post($arrayData, $url)
     {
-        $logs_path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'logs';
-        if(!file_exists($logs_path)) {
-            mkdir($logs_path);
-        }
+        date_default_timezone_set('Asia/Jakarta');
 
         try {
+
+            $logs_path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'logs';
+            if(!file_exists($logs_path)) {
+                mkdir($logs_path);
+            }
+
             $json = json_encode($arrayData);
             $url =  self::$baseurl . $url;
 
@@ -26,17 +29,25 @@ class httpRequest extends mobilepulsa {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             $fp = fopen($logs_path . DIRECTORY_SEPARATOR . 'errorlog.txt', 'a');
+            fwrite($fp, "\n=========== ". date('d/m/Y H:i:s') ." ============\n");
             curl_setopt($ch, CURLOPT_VERBOSE, 1);
             curl_setopt($ch, CURLOPT_STDERR, $fp);
             $data = json_decode(curl_exec($ch), 1);
+
+            if(curl_errno($ch)) {   
+                fwrite($fp, curl_error($ch) . ' - at ' . date('d - m - Y'));
+                throw new Exception(curl_error($ch));
+            }
+
             curl_close($ch);
-            fwrite($fp, "##############################################\n");
 
             $data['env'] = self::$env;
             return $data;
+
         } catch (\Throwable $th) {
-            throw new Exception($th);
+            throw new Exception($th->getMessage());
         }
     }
 }
